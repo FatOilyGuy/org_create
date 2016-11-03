8a158fcc027e6877b95ee501365a2b08c3470fea
Timotei Manolache <timotei.manolache@ro.ibm.com>
2016-11-03 17:30:01 +0200

    Whatever

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+console.log("ce mortii ma-tii");
+  function formValitator() {
+    var emailInput = document.forms['form']['email'].value;
+    console.log(emailInput);
+  }
+};
+
+//add .invalid input pe input
+//remove hidden from glyphicon
+//figure out how to tooltip
